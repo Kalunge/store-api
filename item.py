9044d227c70253b95e9ec94037e018d726e4658a
Kalungez <muthomititus+github@gmail.com>
2020-03-16 18:20:13 +0300

    add ability to register users, add edit and deletefrom or to the database

---
+++ item.py
@@ -0,0 +1,67 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+import sqlite3
+
+items = []
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='this field cannot be left blank'
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message':'that item does not exist'}, 404
+
+    @classmethod
+    def find_by_name(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name, ))
+        row = result.fetchone()
+        if row:
+            return {'item':{'name':row[0], 'price':row[1]}}
+
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'item':f'an item with the name {name} already exists'}, 400
+        data = Item.parser.parse_args()
+        item = {
+            'name' : name, 'price':data['price']
+        }
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES(?,?)"
+        cursor.execute(query, (data['name'], data['price']))
+
+        return item
+
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+        return {'message' : 'item deleted successfully'}, 200
+
+    def put(self, name):
+        data = Item.parser.parse_args() 
+        item = next(filter(lambda x:x['name'] == name, items), None)
+        if item is None:
+            item = {'name' : name, 'price' : data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User():
     def __init__(self, _id, username, password):
@@ -38,6 +39,32 @@ class User():
         conn.close()
 
         return user
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str, 
+        required=True,
+        help='This field cannot be left blank'
+    )
+    parser.add_argument('password',
+        type=str, 
+        required=True,
+        help='This field cannot be left blank'
+    )
+    
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        if User.find_by_username(data['username']):
+            return {'message':'a username by that name already exists'}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users Values(NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+        connection.commit()
+        connection.close()
+        return {'message':'user successfullt created'}, 201
         
         
 
